e1caf0a2ad1d9aeebb61a15968d7d126db91199d
admin01 <paradoxraphael@gmai.com>
2024-11-21 14:54:17 +1100

    add route and controller to handle post request and test with postman

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,15 @@
 exports.getPosts = (req, res, next) => {
-  res.json({ title: "First Post" });
+  res
+    .status(200) //indicate success
+    .json({ title: "First Post", content: "This is the first post" });
+};
+
+exports.postPosts = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res.status(201).json({
+    //indicate success and create resources
+    message: "post created successfully!",
+    post: { id: new Date().toISOString(), title, content },
+  });
 };
